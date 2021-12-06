5f23bd8c6c79db3ac64eac072c54666389c5ba7b
Renchuan.Xiao <569053449@qq.com>
2021-12-06 13:28:15 +0800

    Ad

--- src/main/java/CleanLog.java
+++ src/main/java/CleanLog.java
@@ -1,7 +1,16 @@
+import org.apache.commons.lang.StringUtils;
+
+import java.util.Locale;
+
 public class CleanLog {
 
     public static void main(String[] args) {
-        String string =FileUtils.ReadFile("C:\\Users\\56905\\Desktop\\AccountManager.java");
-        System.out.println(string);
+        String str = "FAFAFCATCH ()";
+        String str1 = "cfaftacfh";
+        Boolean flag =str.contains("catch ()");
+        int i = StringUtils.indexOf(str.toLowerCase(Locale.ROOT),"catch ()");
+        int b = StringUtils.indexOf("catch", str1);
+         RandomAccessFileUtils.modifyFileContent("D:\\Etix-Repository\\etix\\web\\src\\main\\java\\com\\etix\\ticketing\\accountManager\\AccountManager.java","e.printStackTrace();","logger.log(Level.WARNING,\"\",e); \n");
+
     }
 }
--- src/main/java/RandomAccessFileUtils.java
+++ src/main/java/RandomAccessFileUtils.java
@@ -1,16 +1,21 @@
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+
 import java.io.*;
+import java.util.Locale;
 
 public class RandomAccessFileUtils {
 
+    private static final org.apache.log4j.Logger LOGGER = Logger.getLogger(FileUtils.class);
 
     /**
-     * @param fileName 文件名称
-     * @param oldstr   要修改的字符串
-     * @param newStr   新的字符串
+     * @param path   文件路径
+     * @param oldstr 要修改的字符串
+     * @param newStr 新的字符串
      * @return
      */
     //  文件修改 文件内容出现换行就会有错误
-    private static boolean modifyFileContent(String path, String oldstr, String newStr) {
+    public static boolean modifyFileContent(String path, String oldstr, String newStr) {
         ///定义一个随机访问文件类的对象
         RandomAccessFile raf = null;
         try {
@@ -20,20 +25,46 @@ public class RandomAccessFileUtils {
             String line = null;
             // 记住上一次的偏移量
             long lastPoint = 0;
-            //循环读出文件内容
+            // 记录当前catch代码块的偏移量
+            long catchPoint = 0;
+            //确认当前catch是否结束
+            int findCatchBlock = 1;
+            boolean replaceFlag = true;
             while ((line = raf.readLine()) != null) {
-                // 文件当前偏移量 返回文件记录指针的当前位置
-                final long point = raf.getFilePointer();
-                // 查找要替换的内容
-                if (line.contains(oldstr)) {
-                    //修改内容,line读出整行数据
-                    String str = line.replace(oldstr, newStr);
-                    //文件节点移动到文件开始
-                    System.out.println(str);
-                    raf.seek(lastPoint);
-                    raf.writeBytes(str);
+                // 查找catch代码块
+                if (line.contains("catch (")) {
+                    while (line != null) {
+                        //截取catch后的字符串
+                        line = StringUtils.substring(line, line.indexOf("catch"));
+                        // 文件当前偏移量 返回文件记录指针的当前位置
+                        catchPoint = raf.getFilePointer();
+                        if (line.contains(("{"))) {
+                            findCatchBlock = findCatchBlock + 1;
+                        }
+                        if (line.contains(("}"))) {
+                            findCatchBlock = findCatchBlock - 1;
+                        }
+                        if (line.contains("e.printStackTrace();")) {
+                            String str = line.replace(oldstr, newStr);
+                            replaceFlag = false;
+                            LOGGER.info("Replace e.printStackTrace() to " + str);
+                        }
+                        if (line.toLowerCase(Locale.ROOT).contains("log")) {
+                            replaceFlag = false;
+                        }
+                        if (findCatchBlock == 0) {
+                            lastPoint=raf.getFilePointer();
+                        }
+                        if (findCatchBlock == 0 && replaceFlag) {
+                            lastPoint = raf.getFilePointer();
+                            raf.seek(catchPoint);
+                            raf.readLine();
+                            raf.writeBytes("logger.log(Level.WARNING,\"\",e)");
+                            //修改内容,line读出整行数据
+                        }
+                        line=raf.readLine();
+                    }
                 }
-                lastPoint = point;//如果文件出现换行,则修改后的节点需要移动到下一行的开头,所以此处会出现错误
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -48,12 +79,13 @@ public class RandomAccessFileUtils {
     }
 
     /**
-     *文件修改
+     * 文件修改
+     *
      * @param filePath 文件名称
      * @param oldstr   要修改的字符串
      * @param newStr   新的字符串
      */
-    private static void autoReplace(String filePath, String oldstr, String newStr) {
+    public static void autoReplace(String filePath, String oldstr, String newStr) {
         //创建文件
         File file = new File(filePath);
         //记录文件长度
@@ -85,4 +117,5 @@ public class RandomAccessFileUtils {
             }
         }
     }
+
 }
